c58124bbb9cd7dd4a66e2209d8a648293d5ed3fd
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-01-31 17:56:40 -0600

    main.c (struct Product): Rename to `struct product'.
      * main.c (struct Product): Rename to `struct product'.
        (fatal, warning, usage, clear_stdin,
        read_single_product, display_products): New fuctions.
        (readProductData): Rename to `read_products'.
        (printProducts): Rename to `display_single_product'.
        (clearScreen): Remove.
      * .gitignore: New file.
      * lib/remaining_days.h: Remove.

--- main.c
+++ main.c
@@ -5,148 +5,288 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iso646.h>
+#include <inttypes.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdarg.h>
 
-#if defined(_WIN32) || defined(_WIN64)
-void clearScreen(void) {
-	system("cls");
-}
-#elif defined(__linux__)
-void clearScreen(void) {
-	system("clear");
-}
-#endif
+/* The name of the program.  */
+#define PROGNAME "expdate"
 
-// Structures
-typedef struct {
-	int day, month, year;
-} ExpirationDate;
+/* Curren time as time since epoch.  */
+static time_t now;
 
-struct Product {
-	char productName[15];
-	int productAmount;
-	ExpirationDate productExpirationDate;
+/* Abstract a product.  */
+struct product
+{
+	/* The name of the product.  */
+  char name[0x64];
+
+	/* The available quantity of products.  */
+  uint32_t quantity;
+
+	/* The price of the product.  */
+  double price;
+
+	/* The time (represented as time since epoch)
+	   when the product will expire.  */
+  time_t expires_at;
 };
 
-#include "lib/remaining_days.h"
-
-// Function prototype declarations
-void readProductData(struct Product *), printProducts(struct Product);
-
-int main(void) {
-	short int productsNumber, counter;
-	int currentDay, currentMonth, currentYear;
-	time_t now;
-	struct tm *currentTime;
-	
-	now = time(NULL);
-	currentTime = localtime(&now);
-	
-	currentDay = currentTime->tm_mday;
-	currentMonth = currentTime->tm_mon + 1;
-	currentYear = currentTime->tm_year + 1900;
-	
-	do {
-		printf("Insert products number: " );
-		scanf("%hi", &productsNumber);
-		if (productsNumber < 0) printf("Error: Negative number.\n");
-	} while (productsNumber < 0);
-
-	// Create a vector of structures
-	struct Product productsVector[productsNumber];
-	
-	// Get data
-	for (counter = 0; counter < productsNumber; ++counter) {
-		clearScreen();
-		printf("CURRENT DATE: %i/%i/%i\n", currentDay, currentMonth, currentYear);
-		printf("Product [%hi]\n", (counter + 1));
-		readProductData(&productsVector[counter]);
-	}
-	
-	// Print data
-	clearScreen();
-	printf("CURRENT DATE: %i/%i/%i\n", currentDay, currentMonth, currentYear);
-	puts("~~~~~~~~~~~~   ~~~~~~~~~~~~~~~     ~~~~~~~~~~~~~~");
-	puts("PRODUCT NAME   EXPIRATION DATE     RAMAINING DAYS");
-	puts("~~~~~~~~~~~~   ~~~~~~~~~~~~~~~     ~~~~~~~~~~~~~~");
-	for (counter = 0; counter < productsNumber; ++counter) {
-		printProducts(productsVector[counter]);
-	}
-	
-	return 0;
+/* Sends an error message to stderr and exits.  Do not include
+   end-of-line character; it is inserted automatically.   */
+static void
+fatal(char const *const format, ...)
+{
+	char buffer[0x256];
+	va_list ap;
+	va_start(ap, format);
+	vsprintf(buffer, format, ap);
+	va_end(ap);
+	fprintf(stderr, "\x1B[1;71m%s:\x1B[0m \x1B[1;31mfatal:\033[0m %s\n",
+						PROGNAME, buffer);
+	exit(EXIT_FAILURE);
 }
 
-void readProductData(struct Product *infoProd) {
-	printf("Name: ");
-	scanf("%s", (*infoProd).productName);
-	
-	do {
-		printf("Amount: ");
-		scanf("%d", &(*infoProd).productAmount);
-		if ((*infoProd).productAmount < 0) printf("Error: Negative number.\n");
-	} while((*infoProd).productAmount < 0);
-	
-	do {
-		printf("Expiration Date (dd mm yyyy): ");
-		scanf("%d %d %d", \
-		&(*infoProd).productExpirationDate.day, \
-		&(*infoProd).productExpirationDate.month, \
-		&(*infoProd).productExpirationDate.year);
-		
-		if ((*infoProd).productExpirationDate.day < 0 || \
-			((*infoProd).productExpirationDate.day > getMonthDays((*infoProd).productExpirationDate.month, \
-			(*infoProd).productExpirationDate.year)) || \
-			(*infoProd).productExpirationDate.month < 0 || \
-			(*infoProd).productExpirationDate.month > 12 || \
-			(*infoProd).productExpirationDate.year < 1900) {
-			printf("Error: Invalid date.\n");
+/* Sends an warning message to stderr.  Do not include
+   end-of-line character; it is inserted automatically.   */
+static void
+warning(char const *const format, ...)
+{
+	char buffer[0x256];
+	va_list ap;
+	va_start(ap, format);
+	vsprintf(buffer, format, ap);
+	va_end(ap);
+	fprintf(stderr, "\x1B[1;71m%s:\x1B[0m \x1B[1;35mwarning:\033[0m %s\n",
+						PROGNAME, buffer);
+}
+
+/* Prints usage message.  */
+static void
+usage(uint32_t const status)
+{
+  if (EXIT_SUCCESS xor status)
+  {
+    fprintf(stderr, "Try '%s --help' for more information.\n", PROGNAME);
+  }
+  else
+  {
+    printf("\
+Usage: %s N\n", PROGNAME);
+
+		fputs("\
+Calculates the expiration date of a sequence of N products\n\
+inserted by the user.\n\
+\n", stdout);
+
+    fputs("\
+  -h, --help                   display this information\n\
+  -v, --version                display current version\n", stdout);
+  }
+
+  exit(status);
+}
+
+/* Clears (``flushes'') the stadard input's buffer.  */
+static void
+clear_stdin(void)
+{
+	char c = fgetc(stdin);
+  while (c not_eq 10)
+	{
+  	c = fgetc(stdin);
+	}
+}
+
+/* Displays a single product to stdout.  */
+static void
+display_single_product(struct product *const p)
+{
+	/* We need the actual current time.  */
+
+	now = time(NULL);
+
+	/* Formatted expiration date.  */
+	char expdate[0x64];
+
+	strftime(expdate, sizeof expdate, "%c", localtime(&p->expires_at));
+
+	/* Holds a string of how the product will be displayed.  */
+	char const *const format = p->expires_at < now ? "%-15s%-30s%s\n" : "%-15s%-30s%lf\n";
+
+	p->expires_at < now
+		? printf(format, p->name, expdate, "EXPIRED")
+		: printf(format, p->name, expdate, difftime(p->expires_at, now) / (60 * 60 * 24));
+}
+
+/* Reads a product pointed by `p'.  */
+static void
+read_single_product(struct product *const p)
+{
+	/* Ask for product's name.  */
+
+	printf("Product name: ");
+	fgets(p->name, sizeof p->name, stdin);
+
+	/* We do not want the end-of-line character,
+	   so put instead a end-of-string one.  */
+
+	p->name[strcspn(p->name, "\n")] = '\0';
+
+	/* Ask for the quantity of products.  */
+
+	do
+	{
+		printf("Quantity: ");
+		scanf("%d", &p->quantity);
+
+		/* Wrong quantity?  */
+
+		if (0 >= p->quantity)
+		{
+			warning("Quantity is an invalid number or could not be parsed correctly. Please try again.");
+			clear_stdin();
 		}
-		
-	} while ((*infoProd).productExpirationDate.day < 0 || \
-		(*infoProd).productExpirationDate.month < 0 || \
-		((*infoProd).productExpirationDate.day > getMonthDays((*infoProd).productExpirationDate.month, \
-		(*infoProd).productExpirationDate.year)) || \
-		(*infoProd).productExpirationDate.month > 12 || \
-		(*infoProd).productExpirationDate.year < 1900);
-	return;
+	}
+	while(0 >= p->quantity);
+
+	/* Ask for product's price.  */
+
+	do
+	{
+		printf("Price: ");
+		scanf("%lf", &p->price);
+
+		/* Wrong price?  */
+
+		if (0 >= p->price)
+		{
+			warning("Price is an invalid number or could not be parsed correctly. Please try again.");
+			clear_stdin();
+		}
+	}
+	while(0 >= p->price);
+
+
+	/* Number of entities parsed by scanf.  */
+	int32_t n_parsed;
+
+	/* Ask for product's expiration date.  */
+
+	do
+	{
+		puts("Expiration date.");
+		printf("(Use ISO 8601 date format `YYYY-MM-DDThh:mm:ss'. E.g., 2023-01-31T15:50:15): ");
+
+		int32_t y, M, d, h, m, s;
+
+		/* Try to parse our date.  */
+
+		n_parsed = scanf("%4d-%2d-%2dT%2d:%2d:%dZ", &y, &M, &d, &h, &m, &s);
+
+		/* Not what we expect.  */
+
+		if (6 not_eq n_parsed)
+		{
+			warning("The date inserted could not be parsed correctly. Please try again.");
+			clear_stdin();
+			continue;
+		}
+
+		/* The time the prodcut will expire represented as a
+		   broken-down time structure.  */
+		struct tm expires_on =
+		{
+			.tm_year = y - 1900,
+			.tm_mon = M - 1,
+			.tm_mday = d,
+			.tm_hour = h,
+			.tm_min = m,
+			.tm_sec = s
+		};
+
+		/* Store time since epoch in current product.  */
+
+		p->expires_at = mktime(&expires_on);
+	}
+	while (6 not_eq n_parsed);
+
+	clear_stdin();
 }
 
-void printProducts(struct Product product) {
-	char *productName;
-	int currentMonth, currentYear, currentDay, expirationDay, expirationMonth, expirationYear;
-	time_t now;
-	struct tm *currentTime;
-	
-	now = time(NULL);
-	currentTime = localtime(&now);
-	
-	currentDay = (*currentTime).tm_mday;
-	currentMonth = (*currentTime).tm_mon + 1;
-	currentYear = (*currentTime).tm_year + 1900;
-	
-	productName = product.productName;
-	expirationDay = product.productExpirationDate.day;
-	expirationMonth = product.productExpirationDate.month;
-	expirationYear = product.productExpirationDate.year;
-	
-	if ((expirationYear < currentYear) || \
-		((currentDay > expirationDay) && \
-		(currentMonth == expirationMonth && \
-		(currentYear == expirationYear))) || \
-		((expirationMonth < currentMonth) && \
-		(expirationYear == currentYear)) ) {
-		printf("%-15s%i/%i/%-16i%s\n", \
-		productName, \
-		expirationDay, \
-		expirationMonth, \
-		expirationYear, \
-		"EXPIRED");
-	} else {
-		printf("%-15s%i/%i/%-16i%i\n", \
-		productName, \
-		expirationDay, \
-		expirationMonth, \
-		expirationYear, \
-		getRemainingDays(product));
+/* Displays `n' products of `products'.  */
+static void
+display_products(struct product *const products, size_t const n)
+{
+	printf("\nCURRENT DATE: %s", ctime(&now));
+
+	puts("~~~~~~~~~~~~   ~~~~~~~~~~~~~~~~~~~~~~~~~     ~~~~~~~~~~~~~~");
+	puts("PRODUCT NAME   EXPIRATION DATE               RAMAINING DAYS");
+	puts("~~~~~~~~~~~~   ~~~~~~~~~~~~~~~~~~~~~~~~~     ~~~~~~~~~~~~~~");
+
+	uint32_t i = 0;
+
+	for (; i < n; ++i)
+	{
+		display_single_product(&products[i]);
 	}
-	return;
+}
+
+/* Reads `n' products into `products'.  */
+static void
+read_products(struct product *const products, size_t const n)
+{
+	int32_t i = 0;
+
+	for (; i < n; ++i)
+	{
+		printf("Local time is %s\n", ctime(&now));
+		printf("Product #%d\n", 1 + i);
+		puts("----------");
+		read_single_product(&products[i]);
+		puts("----------");
+	}
+}
+
+int32_t
+main(int32_t argc, char *argv[])
+{
+	/* We need the number of products to read.  */
+
+	if (argc < 2)
+	{
+		usage(EXIT_SUCCESS);  /* need some help?  */
+	}
+
+	/* The number of products to ask for.  */
+	uint32_t const n_products = atoi(argv[1]);
+
+	if (0 == n_products)
+	{
+		fatal("The number of products inserted was 0 or could not parse insertion.");
+	}
+	else if (0 > n_products)
+	{
+		fatal("Invalid number of products.");
+	}
+ 
+	/* Collection of products.  */
+	struct product *const products = (struct product *)malloc(sizeof(struct product[n_products]));
+
+  /* Start up time.  */	
+	now = time(NULL);
+
+	/* Ask for products to user.  */
+
+	read_products(products, n_products);
+
+	/* Display products.  */
+
+	display_products(products, n_products);
+
+	free(products);
+	
+	return EXIT_SUCCESS;
 }
